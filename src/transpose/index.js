e57e84e92b5885c2ec8791b09bbeec2c3246b050
Odusina-Adeyemi-Sodiq <odusina.adeyemi@gmail.com>
2023-01-30 13:06:41 +0100

    transpose by Sodiq

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,36 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+// function transpose(array) {
+
+    // function transpose(array) {
+    //     return array[0].map((col, i) => array.map(row => row[i]));
+    //   }
+      
+    //   function transpose(array) {
+    //     return array[0].map((col, c) => array.map((row, r) => array[r][c]));
+    //   }
+
+    // function transpose(array) {
+       
+    //     number = array.reduce((prev, next) => next.map((item, i) =>
+    //       (prev[i] || []).concat(next[i])
+    //     ), []);
+    //     return number
+    //   }
+
+      function transpose(array) {
+        const rows = array.length, cols = array[0].length; 
+        const grid = []; 
+        for (let j = 0; j < cols; j++) { 
+            grid[j] = Array(rows);
+         } 
+         for (let i = 0; i < rows; i++) {
+         for (let j = 0; j < cols; j++) {
+            grid[j][i] = array[i][j]; 
+            } 
+        } 
+            return grid;
+        }
 
 module.exports = transpose;
